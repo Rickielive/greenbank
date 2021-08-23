ce817fde935d2b32122d9d1f29947536c8ac1d10
Rickielive <wachmureithi@gmail.com>
2021-08-23 15:06:15 +0300

    2nd Commit Styling NavBar

--- greenbank-rl/src/App.js
+++ greenbank-rl/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { NavBar } from './components/NavBar/idex'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 
 function App() {
   return (
-    <div className="App">
-      <h1> Rickielive is back!!!!</h1>
-    </div>
+    <Router>
+      <NavBar />
+    </Router>
   );
 }
 
