3116be91ca9ded0efb77041b69ac64f8b077a82b
Rickielive <wachmureithi@gmail.com>
2021-08-27 09:31:44 +0300

    4th Commit Styling Hero Section

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavBar } from './components/NavBar/idex'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+    </Router>
+  );
+}
+
+export default App;
